fb6a25432ea091b1bb66ccea8dfe35851d39671a
gzr7702 <rgraziano.gzr7702@gmail.com>
2019-10-02 19:03:06 -0400

    add Book

--- src/Book.js
+++ src/Book.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-// TODO 2: account for books with no images
 
 class Book extends Component {
      static propTypes = {
